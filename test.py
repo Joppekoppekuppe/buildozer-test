5e35272180c859d02c5b747829a588f0f1edaf56
Joppekoppekuppe <150146097+Joppekoppekuppe@users.noreply.github.com>
2023-11-12 07:43:43 +0200

    Update and rename ruutuaika.py to test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-class Ruutuaika(App):
+class Test(App):
     def build(self):
         return Label(text='hei')
-ruutuaika = Ruutuaika()
-ruutuaika.run()
+test = Test()
+test.run()
